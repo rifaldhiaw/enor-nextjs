2e79bb42d974e1b4b99a8d57cd82695743755423
rifaldhiaw <rifaldhiaw@gmail.com>
2023-01-03 21:38:01 +0700

    add index page of team route

---
+++ src/pages/team/index.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mantine/core";
+
+import { DiscussionLayout } from "~/components/team/TeamLayout";
+
+export const Channel = () => {
+  return (
+    <DiscussionLayout navTitle="Discussion">
+      <Box h="100%" bg="white"></Box>
+    </DiscussionLayout>
+  );
+};
+
+export default Channel;
